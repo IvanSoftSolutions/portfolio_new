6b598442946100895f995ae6f7a9cf193bc4e835
ivand <ivan.daniel.hdz@gmail.com>
2023-11-27 23:10:43 -0600

    Add CV page

---
+++ src/app/cv/page.js
@@ -0,0 +1,139 @@
+'use client'
+import Link from 'next/link'
+import styles from './page.module.css'
+import Image from 'next/image'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconContext } from 'react-icons';
+import { faSquareJs, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase, faAt, faMobile, faGlobe, faLocationDot, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { RiEnglishInput } from "react-icons/ri";
+
+export default function cvPage() {
+    return (
+        <>
+            <header>
+                <div className={styles.header}>
+                    <Link href="/"><span>&lt;-</span> Atrás</Link>
+                </div>
+            </header>
+            <div className={styles.main}>
+                <div className={styles.center}>
+                    <div className={styles.gridCV}>
+                        <div className={styles.CVGridLeft}>
+                            <Image
+                                src="/1.jpg"
+                                alt="Next.js Logo"
+                                // fill={true}
+                                width={255}
+                                height={255}
+                                priority
+                            />
+                            <h2>Contact</h2>
+                            <div className={styles.contactCV}>
+                                <FontAwesomeIcon icon={faAt} style={{ height: "1.5em" }} bounce />
+                                <p>ivan.daniel.hdz@gmail.com</p>
+                            </div>
+                            <div className={styles.contactCV}>
+                                <FontAwesomeIcon icon={faMobile} style={{ height: "1.5em" }} bounce />
+                                <p>(+52) 464 103-95-85</p>
+                            </div>
+                            <div className={styles.contactCV}>
+                                <FontAwesomeIcon icon={faGlobe} style={{ height: "1.5em" }} bounce />
+                                <p>www.sofsolutions.com.mx</p>
+                            </div>
+                            <div className={styles.contactCV}>
+                                <FontAwesomeIcon icon={faLocationDot} style={{ height: "1.5em" }} bounce />
+                                <p>Salamanca, GTO, Mex</p>
+                            </div>
+                            <hr />
+                            <h2>Educacion</h2>
+                            <p>Universidad de Guanajuato</p>
+                            <p>Bachelor's Degree</p>
+                            <p>
+                                <FontAwesomeIcon icon={faGraduationCap} style={{ height: "1.5em" }} bounce /> (01 - 2022)</p>
+                        </div>
+                        <div className={styles.CVGridRight}>
+                            <h1>Ivan Daniel Hernandez Rocha</h1>
+                            <hr />
+                            <h2>About Me</h2>
+                            <p>Decidido, curioso, jovial y emprendedor. Analítico y
+                                perfeccionista, no me gusta la conformidad. Siempre busco
+                                mejorar y estar en movimiento porque me aburro con
+                                facilidad. Tengo facilidad para poner mis pensamientos en
+                                palabras y comunicar intenciones, sentimientos y asuntos,
+                                ya sea de manera verbal o escrita. Me apasionan los
+                                rompecabezas, el volleyball y descubrir cómo funcionan las
+                                cosas.
+                            </p>
+                            <hr />
+                            <h2>Experiencia</h2>
+                            <h3>React Developer</h3>
+                            <p>(01/2022–07/2022)</p>
+                            <p><strong>Utopia Leathers MX</strong> – Desarrollo de una aplicación web y
+                                base de datos para control de inventario y administración.
+                                Desarrollada con React y MySQL, hosted en AWS.
+                            </p>
+                            <h3>Contratista Independiente</h3>
+                            <p>(07/2022– hasta la fecha)</p>
+                            <p><strong>Rev.com</strong> – Transcripciones de audio y video en inglés.</p>
+                            <hr />
+                            <h2>Habilidades</h2>
+                            <div>
+                                <div className={styles.tubes}>
+                                    <div className={styles.tube} style={{ "--percent": 75, "--fill-color": '#1747a1' }}>
+                                        <i className={styles.cap}></i><i className={styles.fill}></i>
+                                        <div className={styles.base}>
+                                            <div className={styles.icon}>
+                                                <FontAwesomeIcon icon={faSquareJs} style={{ height: "2.5em", color: "#1747a1" }} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.tube} style={{ "--percent": 75, "--fill-color": "#61dbfb" }}>
+                                        <i className={styles.cap}></i><i className={styles.fill}></i>
+                                        <div className={styles.base}>
+                                            <div className={styles.icon}>
+                                                <FontAwesomeIcon icon={faReact} style={{ height: "2.5em", color: "#61dbfb" }} />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.tube} style={{ "--percent": 75, "--fill-color": "#3C873A" }}>
+                                        <i className={styles.cap}></i><i className={styles.fill}></i>
+                                        <div className={styles.base}>
+                                            <div className={styles.icon}>
+                                                <FontAwesomeIcon icon={faNodeJs} style={{ height: "2.5em", color: "#3C873A" }} />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.tube} style={{ "--percent": 60, "--fill-color": "#303030" }}>
+                                        <i className={styles.cap}></i><i className={styles.fill}></i>
+                                        <div className={styles.base}>
+                                            <div className={styles.icon}>
+                                                <FontAwesomeIcon icon={faDatabase} style={{ height: "2.5em", color: "#303030" }} />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.tube} style={{ "--percent": 90, "--fill-color": "#B31942" }}>
+                                        <i className={styles.cap}></i><i className={styles.fill}></i>
+                                        <div className={styles.base}>
+                                            <div className={styles.icon}>
+                                                <IconContext.Provider value={{ size: "2.25em", color: "#B31942" }}>
+                                                    <div>
+                                                        <RiEnglishInput />
+                                                    </div>
+                                                </IconContext.Provider>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from 'react';
+import Link from 'next/link'
 import axios from 'axios';
 import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
@@ -11,7 +12,7 @@ import Collapse from '@mui/material/Collapse';
 import Image from 'next/image'
 import styles from './page.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faNodeJs, faReact, faPython, faAws, faApple, faAndroid, faGithub, faTwitter, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import { faSquareJs, faNodeJs, faReact, faPython, faAws, faApple, faAndroid, faGithub, faTwitter, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { faDatabase, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
@@ -61,159 +62,169 @@ export default function Home() {
   }
 
   return (
-    <main className={styles.main}>
-
-      <div className={styles.center}>
-        <div className={styles.title}>
-          <h1>SoftSolutions</h1>
-          <h2>Soluciones suaves para problemas dificiles</h2>
+    <>
+      <header>
+        <div className={styles.header}>
+          <a href="#projects">Proyectos</a>
+          <a href="#contact">Contacto</a>
+          <Link href="/cv">CV</Link>
         </div>
-      </div>
+      </header>
+      <main className={styles.main}>
+        <section id='intro'>
+          <div className={styles.center}>
+            <div className={styles.title}>
+              <h1>SoftSolutions</h1>
+              <h2>Soluciones suaves para problemas dificiles</h2>
+            </div>
+          </div>
+          <div className={styles.center}>
+            <div className={styles.grid2}>
+              <div>
+                <h1> Iván Daniel Hernández Rocha </h1>
+                <h3>Ing. en Sistemas Computacionales </h3>
+                <h3>Desarrollador de Software</h3>
+              </div>
+              <div>
+                <Image
+                  src="/1.jpg"
+                  alt="Next.js Logo"
+                  // fill={true}
+                  width={255}
+                  height={255}
+                  priority
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id='stack'>
+          <div className={styles.gridStack}>
+            <FontAwesomeIcon icon={faSquareJs} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faNodeJs} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faReact} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faPython} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faDatabase} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faAws} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faApple} style={{ height: "5em" }} />
+            <FontAwesomeIcon icon={faAndroid} style={{ height: "5em" }} />
+          </div>
+        </section>
+        <section id='projects'>
+          <div className={styles.center}>
+            <div className={styles.subtitle}>
+              <h2>Proyectos</h2>
+            </div>
+          </div>
 
-      <div className={styles.center}>
-        <div className={styles.grid2}>
-          <div>
-            <h1> Ivan Daniel Hernández Rocha </h1>
-            <h3>Ing. en Sistemas Computacionales </h3>
-            <h3> Desarrollador de Software</h3>
+          <div className={styles.center}>
+            <div className={styles.card}>
+              <a
+                href="https://utopia-rnttb2v36-ivansoftsolutions-projects.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>
+                  Utopia WebApp <span>-&gt;</span>
+                </h2>
+                <p>Una aplicación de administracion y control de inventario para una compañía de pieles.</p>
+                <p>React, MongoDB </p>
+              </a>
+              <a
+                href="https://github.com/IvanSoftSolutions/utopia_new"
+                target='_blank'>
+                <FontAwesomeIcon icon={faGithub} style={{ height: "2em" }} />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section id='contact'>
+          <div className={styles.center}>
+            <div className={styles.subtitle}>
+              <h2>Contacto</h2>
+            </div>
+          </div>
+          <div className={styles.grid}>
+            <div>
+              <a href="https://twitter.com/_SoftSolutions" target='_blank'>
+                <FontAwesomeIcon icon={faTwitter} style={{ height: "2em" }} />
+              </a>
+              <p>@_SoftSolutions</p>
+            </div>
+            <div>
+              <a href="https://www.instagram.com/softsolutionsmx/" target='_blank'>
+                <FontAwesomeIcon icon={faInstagram} style={{ height: "2em" }} />
+              </a>
+              <p>softsolutionsmx</p>
+            </div>
+            <div>
+              <a href="https://www.tiktok.com/@softsolutions_mx" target='_blank'>
+                <FontAwesomeIcon icon={faTiktok} style={{ height: "2em" }} />
+              </a>
+              <p>softsolutions_mx</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faEnvelope} style={{ height: "2em" }} />
+              <p>softsolutions4@outlook.com</p>
+            </div>
+          </div>
+          <div className={styles.grid}>
+          </div>
+
+          <div className={styles.center}>
+            <div className={styles.subtitle}>
+              <h2>Escribenos</h2>
+            </div>
           </div>
           <div>
-            <Image
-              src="/1.jpg"
-              alt="Next.js Logo"
-              // fill={true}
-              width={255}
-              height={255}
-              priority
-            />
+            <form className={styles.contact} onSubmit={sendMail}>
+              <input className={styles.contactChild} type="text" name="name" placeholder="Nombre" value={name} onChange={handleNameChange} />
+              <input className={styles.contactChild} type="email" name="email" placeholder="Correo Electronico" value={email} onChange={handleEmailChange} />
+              <textarea className={styles.contactChild} name="message" rows="5" cols="55" placeholder="Mensaje" value={message} onChange={handleMessageChange}></textarea>
+              <button className={styles.contactChild} type='submit'>Enviar</button>
+            </form>
+            <Box sx={{ width: '100%' }}>
+              <Collapse in={openSuccess}>
+                <Alert variant="filled" severity="success" action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setOpenSuccess(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }>
+                  <AlertTitle>Tu mensaje ha sido enviado.</AlertTitle>
+                  En breve nos pondremos en contacto contigo.
+                </Alert>
+              </Collapse>
+            </Box>
+            <Box sx={{ width: '100%' }}>
+              <Collapse in={openError}>
+                <Alert variant="filled" severity="error" action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setOpenError(false);
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }>
+                  <AlertTitle> Mensaje no enviado.</AlertTitle>
+                  Por favor llena todos los campos
+                </Alert>
+              </Collapse>
+            </Box>
           </div>
-        </div>
-      </div>
+        </section>
 
-      <div className={styles.gridStack}>
-        <FontAwesomeIcon icon={faJs} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faNodeJs} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faReact} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faPython} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faDatabase} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faAws} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faApple} style={{ height: "5em" }} />
-        <FontAwesomeIcon icon={faAndroid} style={{ height: "5em" }} />
-      </div>
-
-      <div className={styles.center}>
-        <div className={styles.subtitle}>
-          <h2>Proyectos</h2>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <div className={styles.card}>
-          <a
-            href="https://utopia-rnttb2v36-ivansoftsolutions-projects.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Utopia WebApp <span>-&gt;</span>
-            </h2>
-            <p>Una aplicación de administracion y control de inventario para una compañía de pieles.</p>
-            <p>React, MongoDB </p>
-          </a>
-          <a
-            href="https://github.com/IvanSoftSolutions/utopia_new"
-            target='_blank'>
-            <FontAwesomeIcon icon={faGithub} style={{ height: "2em" }} />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <div className={styles.subtitle}>
-          <h2>Contacto</h2>
-        </div>
-      </div>
-      <div className={styles.grid}>
-        <div>
-          <a href="https://twitter.com/_SoftSolutions" target='_blank'>
-            <FontAwesomeIcon icon={faTwitter} style={{ height: "2em" }} />
-          </a>
-          <p>@_SoftSolutions</p>
-        </div>
-        <div>
-          <a href="https://www.instagram.com/softsolutionsmx/" target='_blank'>
-            <FontAwesomeIcon icon={faInstagram} style={{ height: "2em" }} />
-          </a>
-          <p>softsolutionsmx</p>
-        </div>
-        <div>
-          <a href="https://www.tiktok.com/@softsolutions_mx" target='_blank'>
-            <FontAwesomeIcon icon={faTiktok} style={{ height: "2em" }} />
-          </a>
-          <p>softsolutions_mx</p>
-        </div>
-        <div>
-          <a href="" >
-            <FontAwesomeIcon icon={faEnvelope} style={{ height: "2em" }} />
-          </a>
-          <p>softsolutions4@outlook.com</p>
-        </div>
-      </div>
-      <div className={styles.grid}>
-      </div>
-
-      <div className={styles.center}>
-        <div className={styles.subtitle}>
-          <h2>Escribenos</h2>
-        </div>
-      </div>
-      <div>
-        <form className={styles.contact} onSubmit={sendMail}>
-          <input className={styles.contactChild} type="text" name="name" placeholder="Nombre y Apellido" value={name} onChange={handleNameChange} />
-          <input className={styles.contactChild} type="email" name="email" placeholder="Correo Electronico" value={email} onChange={handleEmailChange} />
-          <textarea className={styles.contactChild} name="message" rows="5" cols="55" placeholder="Mensaje" value={message} onChange={handleMessageChange}></textarea>
-          <button className={styles.contactChild} type='submit'>Enviar</button>
-        </form>
-        <Box sx={{ width: '100%' }}>
-          <Collapse in={openSuccess}>
-            <Alert variant="filled" severity="success" action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpenSuccess(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }>
-              <AlertTitle>Tu mensaje ha sido enviado.</AlertTitle>
-              En breve nos pondremos en contacto contigo.
-            </Alert>
-          </Collapse>
-        </Box>
-        <Box sx={{ width: '100%' }}>
-          <Collapse in={openError}>
-            <Alert variant="filled" severity="error" action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpenError(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }>
-              <AlertTitle> Mensaje no enviado.</AlertTitle>
-              Por favor llena todos los campos
-            </Alert>
-          </Collapse>
-        </Box>
-      </div>
-
-    </main>
+      </main>
+    </>
   )
 }
